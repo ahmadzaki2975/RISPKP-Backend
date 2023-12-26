77ce005f6ec134379d1dbe912c38dbf1d209ddc5
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-26 21:09:08 +0700

    fix: bad request prevention

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/user").User;
 const express = require("express");
 
 const userController = express.Router();
+const CustomError = require("../errors/CustomError");
 
 const getAllUsers = async (req, res) => {
   try {
@@ -16,10 +17,21 @@ const getAllUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
   try {
-    const user = new User(req.body);
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new CustomError("Username and password are required", 400);
+    }
+    const user = new User({
+      username,
+      password,
+    });
     await user.save();
     res.send(user);
   } catch (error) {
+    console.log(error.message);
+    if(error instanceof CustomError) {
+      return res.status(error.status).send(error.toResponse());
+    }
     res.status(500).send({
       message: error.message,
     });
