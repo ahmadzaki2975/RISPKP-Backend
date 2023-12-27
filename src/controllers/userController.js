134178268fa4e4cb96ebfb294acd5e3c3c24776b
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-27 07:17:16 +0700

    feat: create login route and controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,9 +4,6 @@ const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
 const CustomError = require("../errors/CustomError");
 
-const userController = express.Router();
-const pepper = process.env.PEPPER;
-
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({});
@@ -16,36 +13,82 @@ const getAllUsers = async (req, res) => {
       message: error.message,
     });
   }
-}
+};
 
 const createUser = async (req, res) => {
-  
   try {
-    if(!pepper) {
-      throw new CustomError("Pepper not found", 500);
-    }
     const { username, password } = req.body;
     if (!username || !password) {
       throw new CustomError("Username and password are required", 400);
     }
+    const salt = await bcrypt.genSalt(10);
     const user = new User({
       username,
-      password: await bcrypt.hash(password + pepper, 10),
+      password: await bcrypt.hash(password, salt),
+      salt,
     });
     await user.save();
     res.send(user);
   } catch (error) {
     console.log(error.message);
-    if(error instanceof CustomError) {
+    if (error instanceof CustomError) {
       return res.status(error.status).send(error.toResponse());
     }
     res.status(500).send({
       message: error.message,
     });
   }
-}
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    if (!username || !password) {
+      throw new CustomError("Username and password are required", 400);
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw new CustomError("User not found", 404);
+    }
+
+    const combinedPassword = user.password + user.salt;
+    await bcrypt.compare(combinedPassword, password, (err) => {
+      if (err) {
+        throw new CustomError("Wrong password", 400);
+      }
+    });
+
+    const token = jsonwebtoken.sign(
+      {
+        username: user.username,
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "8h" }
+    );
+
+    res
+      .cookie("token", token, { maxAge: 8 * 3600000 })
+      .cookie("set-date", new Date().toISOString())
+      .status(200)
+      .send({
+        message: "Login successful",
+      });
+  } catch (error) {
+    console.log(error.message);
+    if (error instanceof CustomError) {
+      return res.status(error.status).send(error.toResponse());
+    }
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   getAllUsers,
   createUser,
+  login,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const { getAllUsers, createUser } = require("../controllers/userController");
+const { getAllUsers, createUser, login } = require("../controllers/userController");
 
 const userRouter = express.Router();
 userRouter.get("/", getAllUsers);
 userRouter.post("/", createUser);
+userRouter.post("/login", login);
 
 module.exports = userRouter;
