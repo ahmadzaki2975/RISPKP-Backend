fc1cac1cec4a335cc9ad263bbc6949bcab067627
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-27 07:41:12 +0700

    fix: cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
@@ -30,7 +30,6 @@ try {
   if (!process.env.MONGO_URI) {
     throw new Error("Database URI can't be found");
   }
-  console.log(process.env.MONGO_URI);
   mongoose.connect(process.env.MONGO_URI);
   mongoose.connection.once("open", () => {
     console.log("Connected to database");
