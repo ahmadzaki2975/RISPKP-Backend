ca62e155a7df8b4e7650957e9c20290492e13949
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-04 17:30:42 +0700

    fix: improve error handling

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,19 +2,19 @@ const jsonwebtoken = require("jsonwebtoken");
 const { User } = require("../models/user");
 
 const authenticateAdmin = async (req, res, next) => {
-  if (typeof res?.headers?.cookie === "undefined") {
+  if (typeof req?.cookies?.token === "undefined") {
     return res.status(401).send({
       message: "Token not found",
     });
   }
-  const token = req.headers.cookie.split("; ")[1].replace("token=", "");
+  const token = req.cookies.token;
   console.log(token);
 
   const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
   console.log(decoded.id);
   const user = await User.findById(decoded.id);
   console.log(user);
-  
+
   if (!user) {
     return res.status(404).send({
       message: "User not found",
@@ -31,26 +31,29 @@ const authenticateAdmin = async (req, res, next) => {
 };
 
 const authenticateUser = async (req, res, next) => {
-  if (typeof res?.headers?.cookie === "undefined") {
+  if (typeof req?.cookies?.token === "undefined") {
     return res.status(401).send({
       message: "Token not found",
     });
   }
-  const token = req?.headers?.cookie?.split("; ")[1]?.replace("token=", "");
+  const token = req.cookies.token;
   console.log(token);
-
-  const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
-  console.log(decoded.id);
-  const user = await User.findById(decoded.id);
-  console.log(user);
-
-  if(!user) {
-    return res.status(404).send({
-      message: "User not found",
+  try {
+    const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+    console.log(decoded.id);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    return res.status(401).send({
+      message: "Invalid token",
     });
   }
 
   next();
 };
 
-module.exports = {authenticateAdmin, authenticateUser};
+module.exports = { authenticateAdmin, authenticateUser };
