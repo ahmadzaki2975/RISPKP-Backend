c040dcbbf803bf55bce0026389dd4ab671fa4afc
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-04 18:26:22 +0700

    fix: retry CORS value

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
     origin:
       process.env.NODE_ENV === "dev"
         ? "http://localhost:3000"
-        : ".vercel.app",
+        : "https://e-rispkp.vercel.app",
     credentials: true,
   })
 );
