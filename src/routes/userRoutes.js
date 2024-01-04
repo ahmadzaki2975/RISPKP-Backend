ff2f6035365abc70658a8bc4862c1d0fe3a21406
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-04 17:30:53 +0700

    feat: add route to get user data

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { getAllUsers, createUser, login } = require("../controllers/userController");
-const authenticateAdmin = require("../middlewares/authentication");
+const { getAllUsers, createUser, login, getUserData } = require("../controllers/userController");
+const {authenticateAdmin, authenticateUser} = require("../middlewares/authentication");
 
 const userRouter = express.Router();
 userRouter.get("/", authenticateAdmin, getAllUsers);
 userRouter.post("/", createUser);
 userRouter.post("/login", login);
+userRouter.get("/data", authenticateUser, getUserData);
 
 module.exports = userRouter;
